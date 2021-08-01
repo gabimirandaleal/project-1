c74bca415e3388469b80cb6caa09bd98d68bb727
Gabriela Leal <gabrielamiranda@alu.ufc.br>
2021-08-01 15:20:54 -0300

    popup na recuperacao de senha

---
+++ Projeto-1/js/pop-up.js
@@ -0,0 +1,7 @@
+function abrir(URL) {
+    window.open(URL, '_self', 'width=795, height=590, top=100, left=699, scrollbars=no, status=no, toolbar=no, location=no, menubar=no, resizable=no, fullscreen=no')
+  }
+
+function abrirPopUp(){
+    document.getElementById('popup').style.display = 'block';
+}
